2c39bda1703339a75a16ebf23d4efa6ba4964f13
mathidachuk <mathidachuk@gmail.com>
2020-07-22 23:26:56 -0700

    resolve non-unique name issue

--- R/hp_datatree2.R
+++ R/hp_datatree2.R
@@ -2,37 +2,9 @@
 # library(dplyr)
 # library(hierplane)
 #
-# acme <- Node$new("Acme Inc.")
-# accounting <- acme$AddChild("Accounting")
-# software <- accounting$AddChild("New Software")
-# standards <- accounting$AddChild("New Accounting Standards")
-# research <- acme$AddChild("Research")
-# newProductLine <- research$AddChild("New Product Line")
-# newLabs <- research$AddChild("New Labs")
-# it <- acme$AddChild("IT")
-# outsource <- it$AddChild("Outsource")
-# agile <- it$AddChild("Go agile")
-# goToR <- it$AddChild("Switch to R")
-#
-# acme$Accounting$`New Software`$cost <- 1000000
-# acme$Accounting$`New Accounting Standards`$cost <- 500000
-# acme$Research$`New Product Line`$cost <- 2000000
-# acme$Research$`New Labs`$cost <- 750000
-# acme$IT$Outsource$cost <- 400000
-# acme$IT$`Go agile`$cost <- 250000
-# acme$IT$`Switch to R`$cost <- 50000
-#
-# acme$Accounting$`New Software`$p <- 0.5
-# acme$Accounting$`New Accounting Standards`$p <- 0.75
-# acme$Research$`New Product Line`$p <- 0.25
-# acme$Research$`New Labs`$p <- 0.9
-# acme$IT$Outsource$p <- 0.2
-# acme$IT$`Go agile`$p <- 0.05
-# acme$IT$`Switch to R`$p <- 1
-#
-# acme$IT$Outsource$AddChild("India")
-# acme$IT$Outsource$AddChild("Poland")
-#
+# data(acme)
+# acme$IT$AddChild("New Labs") # make dupe name for test
+# acme$IT$AddChild("Here/There") # what if theres a slash?
 # acme$Set(type = c('company', 'department', 'project', 'project', 'department', 'project', 'project', 'department', 'program', 'project', 'project', 'project', 'project'))
 
 dt_root <- function(x) {
@@ -60,24 +32,31 @@ dt_collect <- function(x) {
 #' @export
 hp_datatree <- function(.data, title = "Hierplane", attributes = NULL, link = "to",
                         styles = NULL) {
+
   requireNamespaceQuietStop("data.tree")
+
+
+  # clean up the slashes
+  .data$Set(name = gsub("[/]", "~~~placeholder~~~", .data$Get("name")))
+
+
   root <- dt_root_df(.data)
   df <- vctrs::vec_rbind(dt_collect(.data), root)
+
 
   # should only expect a single NA in the link column, this is NA comes
   # from binding the root dataframe to the tree. If there are multiple
   # NAs, then the there are missing links
-  missing_links <- "TRUE" %in% names(table(is.na(df[[link]])))
-  if (missing_links) {
-    if (table(is.na(df[[link]]))[["TRUE"]] > 1) {
+  missing_links <- sum(is.na(df[[link]]))
+  if (missing_links > 1) {
       warning(paste0(
         "There are >1 missing values in the link column [", link, "]",
         "\n* Only the row containing the root can have a missing link value",
         "\n* Setting link to column [to]"
       ), call. = FALSE)
       link <- "to"
-    }
   }
+
 
   # Links really shouldn't refer to the parent, otherwise the hierplane
   # becomes distorted, warn the user when this happens and set the link
@@ -91,7 +70,13 @@ hp_datatree <- function(.data, title = "Hierplane", attributes = NULL, link = "t
   }
 
   df[[link]] <- ifelse(is.na(df[[link]]), root$from, df[[link]])
-  print(df)
+
+  # Deal with duplicated child_id
+  df$child <- gsub(".*[/]", "", df[["to"]])
+  # change all the `----` back to `/`
+  df$from <- gsub("~~~placeholder~~~", "/", df$from)
+  df$to <- gsub("~~~placeholder~~~", "/", df$to)
+  df$child <- gsub("~~~placeholder~~~", "/", df$child)
 
   hp_dataframe(
     .data = df,
@@ -100,7 +85,7 @@ hp_datatree <- function(.data, title = "Hierplane", attributes = NULL, link = "t
     settings = hierplane_settings(
       parent_id = "from",
       child_id = "to",
-      child = "to",
+      child = "child",
       root_tag = root$from,
       node_type = "from",
       link = link,
@@ -108,7 +93,7 @@ hp_datatree <- function(.data, title = "Hierplane", attributes = NULL, link = "t
     )
   )
 }
-
+#
 # # hierplane
 # acme %>%
 #   hp_datatree(
@@ -121,4 +106,4 @@ hp_datatree <- function(.data, title = "Hierplane", attributes = NULL, link = "t
 #     width = "auto",
 #     height = "auto"
 #   )
-
+#
